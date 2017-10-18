982e5f0ca275c0eb8bd570e3f70ea3a990bb7200
zzl <525966315@qq.com>
2017-10-18 10:46:21 +0800

    表结构更改

--- src/main/java/com/user/Entity/User.java
+++ src/main/java/com/user/Entity/User.java
@@ -9,6 +9,12 @@ public class User {
 	private String id;
 	private String account;
 	private String pass;
+	private String eamil;
+	private String name;
+	private String province;
+	private String city;
+	private String photo;
+	
 	public String getId() {
 		return id;
 	}
@@ -17,6 +23,36 @@ public class User {
 	}
 	public String getAccount() {
 		return account;
+	}
+	public String getEamil() {
+		return eamil;
+	}
+	public void setEamil(String eamil) {
+		this.eamil = eamil;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getProvince() {
+		return province;
+	}
+	public void setProvince(String province) {
+		this.province = province;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
 	}
 	public void setAccount(String account) {
 		this.account = account;
