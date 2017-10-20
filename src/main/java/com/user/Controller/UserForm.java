7282d5e8c87d23aabef78fcddd2a2371df72ce7e
zzl <525966315@qq.com>
2017-10-20 10:58:01 +0800

    OK

--- src/main/java/com/user/Controller/UserForm.java
+++ src/main/java/com/user/Controller/UserForm.java
@@ -1,5 +1,25 @@
 package com.user.Controller;
 
-public class UserForm {
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+public class UserForm {
+	@NotNull(message="不能为空")
+	@Size(min=3,message="不少于3个字符")
+	private String account;
+	@NotNull(message="不能为空")
+	@Size(min=3,message="不少于3个字符")
+	private String pass;
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
 }
