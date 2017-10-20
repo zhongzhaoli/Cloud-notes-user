5d0eab3bbf33ba91ac2bff48cde1ec5727616c6e
zzl <525966315@qq.com>
2017-10-20 10:51:50 +0800

    新版

--- src/main/java/com/user/Controller/UserController.java
+++ src/main/java/com/user/Controller/UserController.java
@@ -2,6 +2,8 @@ package com.user.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 @Controller
 public class UserController {
 	
@@ -12,10 +14,22 @@ public class UserController {
 		return "login";
 	}
 	
+	//注册页面
+	@RequestMapping("/register")
+	public String register(){
+		return "register";
+	}
+	
 	//第三方登录，
 	@RequestMapping("/wxine")
 	public String wxine(){
 		return "wxine";
+	}
+	
+	//注册
+	@RequestMapping(value="/user",method=RequestMethod.POST)
+	public String register_in(){
+		return "login";
 	}
 }
 	
