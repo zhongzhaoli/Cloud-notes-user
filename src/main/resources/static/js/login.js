89c3a7b3f10ff69b86ddad7d4a941bb3ede198b6
zzl <525966315@qq.com>
2017-10-23 15:06:39 +0800

    login register

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,4 +1,6 @@
 function succ_an(){
+	$('.header_text')[0].children[1].remove();
+	$('.header_text')[0].children[1].remove();
 	$('form').fadeOut(500);
 	if ($('.wrapper')[0]) {
 		$('.wrapper').addClass('form-success');
@@ -7,8 +9,5 @@ function succ_an(){
 	else {
 		$('.wrapper_register').addClass('form-success');
 		$("#head_banner_h1")[0].innerHTML="欢迎";
-		setTimeout(function(){
-			location.href="/login";
-		},2000)
 	}
-};
+	};
